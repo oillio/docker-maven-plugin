4e26ebd20d73e0fa688bf3bd2dff094f8cdf8393
Dan Jasek <djasek@qualys.com>
2014-06-16 20:14:47 -0600

    Update integration test to use updated Docker link functionality.

--- src/it/build-test-it/src/main/docker/app/run.sh
+++ src/it/build-test-it/src/main/docker/app/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -eux
 
-sed -i "s/localhost:3306/$EXAMPLE_MYSQL_PORT_3306_TCP_ADDR:3306/" hello-world.yml
-
 java -jar example-1.0-SNAPSHOT.jar db migrate hello-world.yml
 java -jar example-1.0-SNAPSHOT.jar server hello-world.yml
